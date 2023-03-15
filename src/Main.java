a4080c9956d8fdb2facbcbdcbac11cfc00bc869c
Олег Спас <spas.oleh@gmail.com>
2023-03-15 21:01:43 +0100

    Code

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 public class Main {
-
     public static void main(String[] args) {
         String text = "Мова Java знайшла широке застосування для програмування браузерів у мережі Інтернет. " +
                 "На ній можна створювати повноцінні програмні продукти - застосунки (applications), а також аплети (applets), що виконуються у мережі Інтернет.";
-        BasicEncoder basicEncoder = new BasicEncoder("Spas Oleh Ihorovuch");
+        BasicEncoder basicEncoder = new BasicEncoder("Spas Oleh Ihorovych");
         String encodedText = basicEncoder.encode(text);
         System.out.println("Text: ");
         System.out.println(text);
@@ -12,5 +11,4 @@ public class Main {
         System.out.println("Decoded: ");
         System.out.println(basicEncoder.decode(encodedText));
     }
-
 }
